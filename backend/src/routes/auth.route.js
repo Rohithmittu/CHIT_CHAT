4fc9c1daace116525378b866cc8b4dc34c9be877
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2025-04-26 18:18:41 +0530

    authentication completed and updating profile pic with cloudinary completed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  checkAuth,
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectedRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -7,6 +14,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-
+router.put("/update-profile", protectedRoute, updateProfile);
+router.get("/check", protectedRoute, checkAuth);
 
 export default router;
